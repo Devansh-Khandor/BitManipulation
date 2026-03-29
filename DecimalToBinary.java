15a97b76b443caeab7d830de862febac26f7055c
Devansh-Khandor <devanshkhandor1234@gmail.com>
2026-03-29 11:54:03 +0530

    ConvertDecimalToBinary

---
+++ DecimalToBinary.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class DecimalToBinary {
+    
+    public static void main(String args[]){
+        System.out.println(convert(13));
+    }
+
+    public static ArrayList<Integer> convert(int x){
+        ArrayList<Integer> list = new ArrayList<>();
+        while(x>1){
+            list.add(x%2);
+            x = x/2;
+        }
+        list.add(x);
+        Collections.reverse(list);
+        return list;
+    }
+}
